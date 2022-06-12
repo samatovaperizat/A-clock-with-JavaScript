3389f8643b22fc8a4a2e8e63b33b8729e1462520
Perizat <samatovaperizat@gmail.com>
2022-06-12 14:31:27 +0200

    done, but the clock is not working, I need to find the bug

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 setInterval(setClock, 1000)
 
-function setClock() {
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
 
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    setRotation(secondHand, secondsRatio)
+    setRotation(secondHand, minutesRatio)
+    setRotation(secondHand, hoursRatio)
 }
+
+function setRotation (element, rotationRatio) {
+   element.style.setProperty('--rotation', rotationRatio * 360) 
+}
+
+setClock()
